8e3b9b7447ff541bd9b32c5ad9e67a99c482a166
brettlangdon <me@brett.is>
2023-12-21 07:55:55 -0500

    print config on startup

--- src/dlhdhr/__main__.py
+++ src/dlhdhr/__main__.py
@@ -5,6 +5,16 @@ from dlhdhr.app import create_app
 
 
 def main() -> None:
+    print("== CONFIG ==")
+    print(f"HOST: {config.HOST}")
+    print(f"PORT: {config.PORT}")
+    print(f"CHANNEL_EXCLUDE: {config.CHANNEL_EXCLUDE}")
+    print(f"CHANNEL_ALLOW: {config.CHANNEL_ALLOW}")
+    print(f"EPG_PROVIDER: {config.EPG_PROVIDER}")
+    if config.EPG_PROVIDER == "epg.best":
+        print(f"EPG_BEST_XMLTV_URL: {config.EPG_BEST_XMLTV_URL}")
+    print("====")
+
     app = create_app()
     uvicorn.run(app, host=config.HOST, port=config.PORT)
 
